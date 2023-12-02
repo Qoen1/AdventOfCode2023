80fc9c19dd2308c9f9828a792ad65ff7c12d26d0
KoenVerstappen <k.verstappen1@student.avans.nl>
2023-12-02 17:41:59 +0100

    dag 2.2

---
+++ dag2.2/Game.cs
@@ -0,0 +1,26 @@
+﻿namespace dag2._2;
+
+public class Game
+{
+    public int Id { get; set; }
+    public int Power
+    {
+        get
+        {
+            int result = 1;
+            foreach (int number in Dice.Values)
+            {
+                result *= number;
+            }
+
+            return result;
+        }
+    }
+
+    public Dictionary<string, int> Dice = new Dictionary<string, int>()
+    {
+        {"red", 0},
+        {"green", 0},
+        {"blue", 0}
+    };
+}
---
+++ dag2.2/Program.cs
@@ -0,0 +1,41 @@
+﻿//read file input.txt
+
+
+using System.Text.RegularExpressions;
+using dag2._2;
+
+Console.WriteLine("Hello World!");
+List<string> lines = File.ReadAllLines("input.txt").ToList();
+
+List<Game> games = new List<Game>();
+int result = 0;
+
+Regex numberRegex = new Regex(@"\d+");
+Regex colorRegex = new Regex(@"red|green|blue");
+
+
+foreach (string line in lines)
+{
+    int id = int.Parse(numberRegex.Matches(line.Substring(0, line.IndexOf(':'))).Single().Value);
+    string coloursString = line.Substring(line.IndexOf(':') + 1);
+    
+    string[] colours = coloursString.Split(new []{';',','}, StringSplitOptions.TrimEntries);
+    
+    Game game = new Game();
+
+    foreach (string colour in colours)
+    {
+        string numberString = numberRegex.Matches(colour).Single().Value;
+        string color = colorRegex.Matches(colour).Single().Value;
+        int number = int.Parse(numberString);
+        if (number > game.Dice[color])
+        {
+            game.Dice[color] = number;
+        }
+    }
+
+    game.Id = id;
+
+    result += game.Power;
+}
+Console.WriteLine($"result: {result}");
